c0357750511f7374d3d3f508b83058499d6fe28e
Bruno Martins <bgcngm@gmail.com>
2023-09-14 18:21:31 +0100

    kernel: Include DTBOs from OEM/ODM folder in base DTB
    It can be the case where OEM/ODM decided to put the main DTBOs
    outside vendor/qcom (e.g. vendor/oplus). Support such usecase
    by generically allowing all .dtbo files found in all first-level
    subdirs to be moved to base DTB folder.
    
    Change-Id: <hash>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -519,6 +519,16 @@ $(INSTALLED_DTBIMAGE_TARGET): $(DTC) $(DTB_OUT)
 ifdef BOARD_DTB_CFG
 	$(MKDTBOIMG) cfg_create $@ $(BOARD_DTB_CFG) -d $(DTB_OUT)/arch/$(KERNEL_ARCH)/boot/dts
 else
+<<<<<<< HEAD
+=======
+ifeq ($(BOARD_USES_QCOM_MERGE_DTBS_SCRIPT),true)
+	$(hide) find $(DTBS_BASE) -type f -name "*.dtb*" | xargs rm -f
+	$(hide) find $(DTBS_OUT) -type f -name "*.dtb*" | xargs rm -f
+	mv $(DTB_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(DTB_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/*/*.dtbo $(DTBS_BASE)/
+	PATH=$(abspath $(HOST_OUT_EXECUTABLES)):$${PATH} python3 $(BUILD_TOP)/vendor/lineage/build/tools/merge_dtbs.py $(DTBS_BASE) $(DTB_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom $(DTBS_OUT)
+	cat $(shell find $(DTB_OUT)/out -type f -name "*.dtb" | sort) > $@
+else
+>>>>>>> 741affad (kernel: Include DTBOs from OEM/ODM folder in base DTB)
 	cat $(shell find $(DTB_OUT)/arch/$(KERNEL_ARCH)/boot/dts -type f -name "*.dtb" | sort) > $@
 endif # BOARD_DTB_CFG
 	$(hide) touch -c $(DTB_OUT)
